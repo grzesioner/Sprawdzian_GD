20eb64f1c48b1bf714c5030ff45cbe1719579682
GD <grzesioner@onet.pl>
2023-11-30 08:39:43 +0100

    Sprawdzian_GD
    : )

--- src/main/java/workshop/Tool.java
+++ src/main/java/workshop/Tool.java
@@ -2,10 +2,12 @@ package workshop;
 
 public class Tool {
     private String toolType;
-    public Tool(String toolType){
-        this.toolType=toolType;
+
+    public Tool(String toolType) {
+        this.toolType = toolType;
     }
-    public void useTool(){
+
+    public void useTool() {
         System.out.println("Narzędzie jest typu: " + toolType);
     }
 }
--- src/main/java/workshop/Worker.java
+++ src/main/java/workshop/Worker.java
@@ -4,10 +4,11 @@ public class Worker {
 
     public static void main(String[] args) {
 
-        Tool[] tools = {new Tool("śrubokrynt"), new Tool("młot") };
+        Tool[] tools = {
+                new Tool("śrubokrynt"), new Tool("młot")
+        };
 
         for (Tool tool : tools) {
-
             tool.useTool();
         }
     }
